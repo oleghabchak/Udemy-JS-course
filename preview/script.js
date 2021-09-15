dc5e927750a37f513b596bc9aebb5a425dc97ec5
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-09-15 16:18:25 +0300

    1

---
+++ preview/script.js
@@ -0,0 +1,9 @@
+const getbtn = document.querySelector('button');
+
+// getbtn.onclick = function() {
+//     alert('hi');
+// };
+getbtn.addEventListener('click', (e) => {
+    console.log(e.target);
+    e.target.remove();
+});
