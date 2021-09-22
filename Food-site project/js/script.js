807d747955549fca5abbe2805a75a1f73da8e1f4
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-09-23 01:14:01 +0300

    create modal window

--- Food-site project/js/script.js
+++ Food-site project/js/script.js
@@ -90,4 +90,26 @@ window.addEventListener('DOMContentLoaded', () =>  {
         }
 
         setClock('.timer', deadline)
+
+
+        //Modal
+
+    const modalTrigger = document.querySelector('[data-modal]'),
+            modal = document.querySelector('.modal'),
+            modalCloseBtn = document.querySelector('[data-close]');
+
+        modalTrigger.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            
+        });
+
+        modalCloseBtn.addEventListener('click', () => {
+            modal.classList.add('hide'),
+            modal.classList.remove('show');
+            
+        });
+
 });
+
+
