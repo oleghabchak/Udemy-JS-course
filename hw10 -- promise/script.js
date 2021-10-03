e26801b2a66c68246b0d172919163bb504710f70
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-04 00:57:03 +0300

    create fetch forma element

--- hw10 -- promise/script.js
+++ hw10 -- promise/script.js
@@ -1,23 +1,36 @@
-'use strict';
+// 'use strict';
 
-console.log('Запит даних');
+// console.log('Запит даних');
 
-const req = new Promise(function( resolve, reject){
-    setTimeout(() => {
-        console.log('підготовка даних...');
+// const req = new Promise(function( resolve, reject){
+//     setTimeout(() => {
+//         console.log('підготовка даних...');
 
-        const product = {
-            name: 'TV',
-            price: 2000
-        };
+//         const product = {
+//             name: 'TV',
+//             price: 2000
+//         };
 
-        resolve(product);
-    }, 2000);
+//         resolve(product);
+//     }, 2000);
+// });
+
+// req.then((product)=>{
+//     setTimeout(() => {
+//         product.status = 'order';
+//         console.log(product);
+//     }, 2000);
+//     });
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({name: 'Alex'}),
+    headers:{
+        'Content-type': 'application/json'
+    }
+})
+    .then((response) => {
+    return response.json();})
+    .then((data) => {
+    console.log(data);
 });
-
-req.then((product)=>{
-    setTimeout(() => {
-        product.status = 'order';
-        console.log(product);
-    }, 2000);
-    });
