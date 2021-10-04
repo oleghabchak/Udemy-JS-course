c960526a004dc959d19b06fb0a2343517f4b3020
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-04 11:58:12 +0300

    array methods

--- hw11-array methods/script.js
+++ hw11-array methods/script.js
@@ -9,10 +9,50 @@ const shortNames = names.filter(function (name) {
 
 console.log(shortNames);
 
+
 // map
 
-const answers = ['IvaN','aNna', "Hello"];
+let answers = ['IvaN','aNna', "Hello"];
 
-const result = answers.map(i => i.toLowerCase());
+answers = answers.map(i => i.toLowerCase());
 
-console.log(result);
+console.log(answers);
+
+
+// ewery/some
+
+const some = [4, 'sdefrsdf', 'h45kk' ];
+
+console.log(some.some(i => typeof(i) === 'number'));
+console.log(some.every(i => typeof(i) === 'number'));
+
+
+// reduce
+
+const arr = [4,5,1,2,6];
+                // 0 + 4
+                // 4 + 5
+                // 9 + 1
+const res = arr.reduce((sum,cur) => sum + cur, 1000);
+console.log(res);
+
+
+const str = ['apple', 'reap', 'pear'];
+
+const result = str.reduce((sum,cur) => `${sum}, ${cur}`);
+console.log(result, str);
+
+
+// example
+
+const obj = {
+    ivan: 'persone',
+    oleg: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(i => i[0]);
+console.log(newArr);
