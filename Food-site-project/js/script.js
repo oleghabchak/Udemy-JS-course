ea5f062ec266f141b8ece5f26bf6cecf3b2018a5
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-16 02:42:07 +0300

    json server

--- Food-site-project/js/script.js
+++ Food-site-project/js/script.js
@@ -210,10 +210,22 @@ window.addEventListener('DOMContentLoaded', () =>  {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async( url, data) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -232,13 +244,8 @@ window.addEventListener('DOMContentLoaded', () =>  {
                 object[key] = value;
             });
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            }).then(data => {
+            postData('http://localhost:3000/requests', JSON.stringify(object))
+            .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
                 statusMessage.remove();
@@ -274,6 +281,6 @@ window.addEventListener('DOMContentLoaded', () =>  {
     }
 });
 
-fetch('http://localhost:3000/menu')
+fetch('http://localhost:3000/requests')
 .then(data => data.json())
 .then(res => console.log(res));
