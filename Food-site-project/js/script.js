fe9ad7f476e0199da5d2d23022b53d91b819cecc
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-10-16 16:25:37 +0300

    get items from json-server

--- Food-site-project/js/script.js
+++ Food-site-project/js/script.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', () =>  {
 
 //TABS
+
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
 
         function hideTabContent() {
+
             tabsContent.forEach(item => {
                 item.classList.add('hide');
                 item.classList.remove('show','fade')
@@ -24,6 +26,7 @@ window.addEventListener('DOMContentLoaded', () =>  {
 
         hideTabContent();
         showTabContent();
+
         tabsParent.addEventListener('click', (event) =>{
             const target = event.target;
             if (target && target.classList.contains('tabheader__item')){
@@ -36,12 +39,11 @@ window.addEventListener('DOMContentLoaded', () =>  {
             }
         });
 
-
         //TIMER
+
         const deadline = '2021-12-31';
 
-        
-    function getTimeRemaining(endtime) {
+        function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
             days = Math.floor( (t/(1000*60*60*24)) ),
             seconds = Math.floor( (t/1000) % 60 ),
@@ -66,6 +68,7 @@ window.addEventListener('DOMContentLoaded', () =>  {
         }
 
         function setClock(selector, endtime) {
+
             const timer = document.querySelector(selector);
                 days = timer.querySelector('#days'),
                 hours = timer.querySelector('#hours'),
@@ -100,6 +103,11 @@ window.addEventListener('DOMContentLoaded', () =>  {
                 btn.addEventListener('click', openModal);
         });
 
+        function closeModal() {
+            modal.classList.toggle('show');
+            document.body.style.overflow = '';
+        } ;
+
         function openModal() {
             modal.classList.add('show');
             modal.classList.remove('hide');
@@ -107,11 +115,7 @@ window.addEventListener('DOMContentLoaded', () =>  {
             document.body.style.overflow = 'hidden';
             clearInterval(modalTimerID);
         };
-        function closeModal() {
-            modal.classList.toggle('show');
-            document.body.style.overflow = '';
-        } ;
-
+        
         modal.addEventListener('click', (e) => {
             if( e.target === modal || e.target.getAttribute('data-close') == '') {
                 closeModal();
@@ -128,16 +132,15 @@ window.addEventListener('DOMContentLoaded', () =>  {
 
 
         function showModalByScroll() {
-            
-                if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+                            if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
                     openModal();
                     window.removeEventListener('scroll', showModalByScroll);
                 };
         };
-
         window.addEventListener('scroll', showModalByScroll);
 
         // Використовуємо класи
+
         class MenuCard {
             constructor(src, alt , title, descr, price , parentSelector, ...classes){
                 this.src = src;
@@ -150,55 +153,64 @@ window.addEventListener('DOMContentLoaded', () =>  {
                 this.transfer = 27;
                 this.changeToUAH();
             }
+            
             changeToUAH() {
             this.price = this.price * this.transfer;
             }
 
             render() {
                 const element = document.createElement('div');
-                this.classes.forEach(className => element.classList.add(className));
+
+                if (this.classes.length === 0) {
+                    this.classes = "menu__item";
+                    element.classList.add(this.classes);
+                } else {
+                    this.classes.forEach(className => element.classList.add(className));
+                }
+    
                 element.innerHTML = `
-                                    <img src=${this.src} alt="${this.alt}>
+                    <img src=${this.src} alt=${this.alt}>
                     <h3 class="menu__item-subtitle">${this.title}</h3>
                     <div class="menu__item-descr">${this.descr}</div>
                     <div class="menu__item-divider"></div>
                     <div class="menu__item-price">
-                        <div class="menu__item-cost">Цiна:</div>
+                        <div class="menu__item-cost">Цена:</div>
                         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                     </div>
-                                `;
+                `;
                 this.parent.append(element);
             }
         }
-
-        new MenuCard(
-            "img/tabs/vegy.jpg",
-            "vegy",
-            'Меню "Фитнес"',
-            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-            9,
-            ".menu .container",
-            "menu__item"
-        ).render();
-        new MenuCard(
-            "img/tabs/post.jpg",
-            "post",
-            'Меню "Постное"',
-            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-            14,
-            ".menu .container",
-            "menu__item"
-        ).render();
     
-        new MenuCard(
-            "img/tabs/elite.jpg",
-            "elite",
-            'Меню “Премиум”',
-            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-            21,
-            ".menu .container",
-            "menu__item"
-        ).render();
+        getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            });
+        });
+
+    //   getResource('http://localhost:3000/menu')
+    //     .then(data => createCard(data));
+
+    // function createCard(data) {
+    //     data.forEach(({img, altimg, title, descr, price}) => {
+    //         const element = document.createElement('div');
+
+    //         element.classList.add("menu__item");
+
+    //         element.innerHTML = `
+    //             <img src=${img} alt=${altimg}>
+    //             <h3 class="menu__item-subtitle">${title}</h3>
+    //             <div class="menu__item-descr">${descr}</div>
+    //             <div class="menu__item-divider"></div>
+    //             <div class="menu__item-price">
+    //                 <div class="menu__item-cost">Цена:</div>
+    //                 <div class="menu__item-total"><span>${price}</span> грн/день</div>
+    //             </div>
+    //         `;
+    //         document.querySelector(".menu .container").append(element);
+    //     });
+    // }
 
     // Forms
 
@@ -225,6 +237,16 @@ window.addEventListener('DOMContentLoaded', () =>  {
         return await res.json();
     };
 
+    async function getResource(url) {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -239,12 +261,9 @@ window.addEventListener('DOMContentLoaded', () =>  {
         
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key){
-                object[key] = value;
-            });
-
-            postData('http://localhost:3000/requests', JSON.stringify(object))
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -280,7 +299,3 @@ window.addEventListener('DOMContentLoaded', () =>  {
         }, 4000);
     }
 });
-
-fetch('http://localhost:3000/requests')
-.then(data => data.json())
-.then(res => console.log(res));
