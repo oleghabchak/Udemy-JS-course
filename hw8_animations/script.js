019d5880d55a3d997e8cd6e88c39877494e583b6
oleghabchak <71442227+oleghabchak@users.noreply.github.com>
2021-11-05 00:17:03 +0200

    animation

--- hw8_animations/script.js
+++ hw8_animations/script.js
@@ -1,25 +1,17 @@
-const btn = document.querySelector('.btn');
-let timerIN,
-i = 0;
-
-function myAnimation() {
-    const elem = document.querySelector('.box');
+const btn = document.querySelector('.btn'),
+        elem = document.querySelector('.box');
     let pos = 0;
 
-    const id = setInterval(frame,10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
+function myAnimation() {
+        pos++;
             elem.style.top = pos + "px";
             elem.style.left = pos + "px";
 
-        }
+    if(pos < 300){
+        requestAnimationFrame(myAnimation);
     }
 }
-
-btn.addEventListener('click',myAnimation);
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
 
 
 
